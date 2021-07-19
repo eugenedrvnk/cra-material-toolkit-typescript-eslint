bf4c238ad94ae4f2debcf10f73884b23ba1267ab
eugene <eugene.topdevs@gmail.com>
2021-07-19 17:15:45 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      Base App Setup
+    </div>
+  );
+}
+
+export default App;
---
+++ src/plugins/muiTheme.ts
@@ -0,0 +1,11 @@
+import { createTheme } from '@material-ui/core';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#0CB1BC',
+    },
+  },
+});
+
+export { theme };
